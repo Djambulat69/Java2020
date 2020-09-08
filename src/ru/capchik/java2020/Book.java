dc4b9edfcd221a30bb387c235053a84583e78ca4
Djambulat <Djambulat69@gmail.com>
2020-09-08 21:38:56 +0300

    IIII

---
+++ src/ru/capchik/java2020/Book.java
@@ -0,0 +1,11 @@
+package ru.capchik.java2020;
+
+public class Book {
+    private int pages;
+    private String title;
+    public Book(int pages, String title)
+    {
+        this.pages = pages;
+        this.title = title;
+    }
+}
