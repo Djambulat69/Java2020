b6a97cbfcf0de1eb98323a5b661d0f848f427d7e
Djambulat <Djambulat69@gmail.com>
2020-09-18 20:01:21 +0300

    Init

--- src/ru/capchik/java2020/Ex2/DogNursery5.java
+++ src/ru/capchik/java2020/Ex2/DogNursery5.java
@@ -1,11 +1,19 @@
 package ru.capchik.java2020.Ex2;
 
+import java.sql.SQLOutput;
+
 public class DogNursery5 {
     private Dog[] nursery = new Dog[5];
     private int lastDog = 0;
     public void addDog(Dog d){
+        if (lastDog < 5) {
             nursery[lastDog] = d;
-            if(lastDog < 5) lastDog++;
+            lastDog++;
+        }
+        else
+        {
+            System.out.println("Nursery is full");
+        }
     }
 
 }
