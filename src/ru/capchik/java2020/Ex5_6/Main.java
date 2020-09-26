791c8f12fbe4610e039f7a5faf4df434fcf73f1f
Djambulat <Djambulat69@gmail.com>
2020-09-26 09:32:03 +0300

    Init

--- src/ru/capchik/java2020/Ex5_6/Main.java
+++ src/ru/capchik/java2020/Ex5_6/Main.java
@@ -2,9 +2,9 @@ package ru.capchik.java2020.Ex5_6;
 
 public class Main {
     public static void main(String[] args) {
-        MovableRectangle mrec = new MovableRectangle("Blue", true,10.0, 16.0, 1, 1);
+        MovableRectangle mrec = new MovableRectangle("Blue", true,10, 10,5,5);
         System.out.println(mrec.toString());
-        mrec.move(5, 6);
+        mrec.move(1, 1);
         System.out.println(mrec.toString());
 
     }
--- src/ru/capchik/java2020/Ex5_6/MovableRectangle.java
+++ src/ru/capchik/java2020/Ex5_6/MovableRectangle.java
@@ -2,15 +2,30 @@ package ru.capchik.java2020.Ex5_6;
 
 public class MovableRectangle extends Rectangle implements Movable {
 
-        private MovablePoint center;
+        private MovablePoint topleft;
+        private MovablePoint downright;
 
-    public MovableRectangle(String color, boolean filled, double width, double length, int x, int y) {
-        super(color, filled, width, length);
-        MovablePoint center = new MovablePoint(x, y);
+    public MovableRectangle(String color, boolean filled, int x1, int y1, int x2, int y2) {
+        super(color, filled, x1 - x2, y1 - y2);
+        topleft = new MovablePoint(x1, y1);
+        downright = new MovablePoint(x2, y2);
     }
 
     @Override
         public void move(int right, int down) {
-            center.move(right, down);
+            topleft.move(right, down);
+            downright.move(right, down);
         }
+
+    @Override
+    public String toString() {
+        return "MovableRectangle{" +
+                "topleft=" + topleft +
+                ", downright=" + downright +
+                ", width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
 }
